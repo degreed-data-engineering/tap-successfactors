a8557e2ae7d8914b07640e88b34d3f274c41e308
Giovanni Bortolussi <gbortolussi@degreed.com>
2023-06-28 08:42:36 +0200

    updates

--- tap_successfactors/streams.py
+++ tap_successfactors/streams.py
@@ -7,38 +7,33 @@ import urllib.parse
 
 from pathlib import Path
 from typing import Dict, Optional, Any, Iterable
-from singer_sdk import typing as th
+from singer_sdk.exceptions import RetriableAPIError
 from singer_sdk.streams import RESTStream
-from singer_sdk.authenticators import APIAuthenticatorBase, BasicAuthenticator
 
 logging.basicConfig(level=logging.INFO)
-
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 
 
-class CustomAuthenticator:
-    def token_request(
-        self, client_id, client_secret, base_url, user_id, company_id, userType
-    ):
-        auth_endpoint = "{}/learning/oauth-api/rest/v1/token".format(base_url)
-        payload = json.dumps(
-            {
-                "grant_type": "client_credentials",
-                "scope": {
-                    "userId": user_id,
-                    "companyId": company_id,
-                    "userType": userType,
-                    "resourceType": "learning_public_api",
-                },
-            }
-        )
-        response = requests.request(
-            "POST",
-            auth_endpoint,
-            data=payload,
-            auth=requests.auth.HTTPBasicAuth(client_id, client_secret),
-        )
-        return response.json()["access_token"]
+def token_request(client_id, client_secret, base_url, user_id, company_id, userType):
+    auth_endpoint = "{}/learning/oauth-api/rest/v1/token".format(base_url)
+    payload = json.dumps(
+        {
+            "grant_type": "client_credentials",
+            "scope": {
+                "userId": user_id,
+                "companyId": company_id,
+                "userType": userType,
+                "resourceType": "learning_public_api",
+            },
+        }
+    )
+    response = requests.request(
+        "POST",
+        auth_endpoint,
+        data=payload,
+        auth=requests.auth.HTTPBasicAuth(client_id, client_secret),
+    )
+    return response.json()["access_token"]
 
 
 class TapSuccessFactorsStream(RESTStream):
@@ -49,7 +44,7 @@ class TapSuccessFactorsStream(RESTStream):
         self.language = self.config["language"]
         self.target_user_id = self.config["target_user_id"]
         if not hasattr(self, "admin_token"):
-            self.admin_token = CustomAuthenticator().token_request(
+            self.admin_token = token_request(
                 self.config["client_id"],
                 self.config["client_secret"],
                 self.config["base_url"],
@@ -58,7 +53,7 @@ class TapSuccessFactorsStream(RESTStream):
                 "admin",
             )
         if not hasattr(self, "user_token"):
-            self.user_token = CustomAuthenticator().token_request(
+            self.user_token = token_request(
                 self.config["client_id"],
                 self.config["client_secret"],
                 self.config["base_url"],
@@ -72,6 +67,47 @@ class TapSuccessFactorsStream(RESTStream):
         """Base URL of source"""
         return self.config["base_url"]
 
+    @property
+    def http_headers(self) -> dict:
+        """Return the http headers needed."""
+        headers = {}
+        headers["Authorization"] = f"Bearer {self.admin_token}"
+        return headers
+
+    def validate_response(self, response):
+        data = response.json()
+        if "error" in data:
+            if (
+                "No search results for provided search criteria"
+                in data["error"]["message"]
+            ):  # mainly used by ScheduledOfferings
+                logging.warning(
+                    f"No search results for provided search criteria. URL: {urllib.parse.unquote(response.request.url)}"
+                )
+                pass
+            elif (
+                "error_description" in data
+                and data["error_description"] == "The token has expired."
+            ):
+                logging.warn("Token expired, refreshing")
+                self.admin_token = token_request(
+                    self.config["client_id"],
+                    self.config["client_secret"],
+                    self.config["base_url"],
+                    self.config["user_id"],
+                    self.config["company_id"],
+                    "admin",
+                )
+                self.user_token = token_request(
+                    self.config["client_id"],
+                    self.config["client_secret"],
+                    self.config["base_url"],
+                    self.config["user_id"],
+                    self.config["company_id"],
+                    "user",
+                )
+                raise RetriableAPIError
+
 
 class Catalogs(TapSuccessFactorsStream):
     name = "catalogs"
@@ -79,13 +115,6 @@ class Catalogs(TapSuccessFactorsStream):
     records_jsonpath = "$.value[0:]"
     path = "/learning/odatav4/public/admin/catalog-service/v1/Catalogs"
     schema_filepath = SCHEMAS_DIR / "catalogs.json"
-
-    @property
-    def http_headers(self) -> dict:
-        """Return the http headers needed."""
-        headers = {}
-        headers["Authorization"] = f"Bearer {self.admin_token}"
-        return headers
 
     def get_child_context(self, record: dict, context: Optional[dict]) -> dict:
         """Return a context dictionary for child streams."""
@@ -105,13 +134,6 @@ class CatalogsCoursesFeed(TapSuccessFactorsStream):
         main_path = "/learning/odatav4/public/admin/catalog-service/v1/CatalogsFeed('{catalog_name}')/CoursesFeed"
         filters = f"?$filter=criteria/localeID eq '{self.language}'"
         return main_path + filters
-
-    @property
-    def http_headers(self) -> dict:
-        """Return the http headers needed."""
-        headers = {}
-        headers["Authorization"] = f"Bearer {self.admin_token}"
-        return headers
 
     def get_child_context(self, record: dict, context: Optional[dict]) -> dict:
         """Return a context dictionary for child streams."""
@@ -136,13 +158,6 @@ class CatalogsCurriculaFeed(TapSuccessFactorsStream):
         filters = f"?$filter=criteria/localeID eq '{self.language}'"
         return main_path + filters
 
-    @property
-    def http_headers(self) -> dict:
-        """Return the http headers needed."""
-        headers = {}
-        headers["Authorization"] = f"Bearer {self.admin_token}"
-        return headers
-
 
 class CatalogsProgramsFeed(TapSuccessFactorsStream):
     parent_stream_type = Catalogs
@@ -157,13 +172,6 @@ class CatalogsProgramsFeed(TapSuccessFactorsStream):
         main_path = "/learning/odatav4/public/admin/catalog-service/v1/CatalogsFeed('{catalog_name}')/ProgramsFeed"
         filters = f"?$filter=criteria/localeID eq '{self.language}'"
         return main_path + filters
-
-    @property
-    def http_headers(self) -> dict:
-        """Return the http headers needed."""
-        headers = {}
-        headers["Authorization"] = f"Bearer {self.admin_token}"
-        return headers
 
 
 class LearningHistorys(TapSuccessFactorsStream):
@@ -182,13 +190,6 @@ class LearningHistorys(TapSuccessFactorsStream):
         )
         return main_path + filters
 
-    @property
-    def http_headers(self) -> dict:
-        """Return the http headers needed."""
-        headers = {}
-        headers["Authorization"] = f"Bearer {self.admin_token}"
-        return headers
-
 
 class ScheduledOfferings(TapSuccessFactorsStream):
     parent_stream_type = CatalogsCoursesFeed
@@ -196,18 +197,6 @@ class ScheduledOfferings(TapSuccessFactorsStream):
     records_jsonpath = "$.value[0:]"
     path = "/learning/odatav4/public/user/learningplan-service/v1/Scheduledofferings?$filter=lisCriteria/itemID eq '{componentID}' and lisCriteria/itemTypeID eq '{componentTypeID}' and lisCriteria/revisionDate eq {revisionDate}"
     schema_filepath = SCHEMAS_DIR / "scheduled_offerings.json"
-
-    def validate_response(self, response):
-        data = response.json()
-        if "error" in data:
-            if (
-                "No search results for provided search criteria"
-                in data["error"]["message"]
-            ):
-                logging.warning(
-                    f"No search results for provided search criteria. URL: {urllib.parse.unquote(response.request.url)}"
-                )
-                pass
 
     @property
     def http_headers(self) -> dict:
@@ -228,10 +217,3 @@ class UserTodoLearningItems(TapSuccessFactorsStream):
         main_path = "/learning/odatav4/public/user/learningplan-service/v1/UserTodoLearningItems"
         filters = f"?$filter=criteria/maxRowNum eq 999999 and criteria/targetUserID eq '{self.target_user_id}'"
         return main_path + filters
-
-    @property
-    def http_headers(self) -> dict:
-        """Return the http headers needed."""
-        headers = {}
-        headers["Authorization"] = f"Bearer {self.admin_token}"
-        return headers
